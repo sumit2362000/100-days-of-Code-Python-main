6c0473f4612c10851b1e408004098a2030f8940e
Harry <harry@codewithharry.com>
2022-02-18 14:26:45 +0530

    https://github.com/sumit2362000/PythonProjects

--- 03-Teasure_Island.py
+++ 03-Teasure_Island.py
@@ -1,4 +1,4 @@
-# Day 3 - Treasure Island
+
 # Control Flow and Logical Operations
 # using flowchart https://app.diagrams.net/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
 
